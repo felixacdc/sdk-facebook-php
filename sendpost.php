f02ec801c1f45d6102781a95c9ec767be338e125
felixacdc <pfmata01@gmail.com>
2016-09-21 11:18:30 -0600

    [modify] fix :smile:

--- sendpost.php
+++ sendpost.php
@@ -16,9 +16,7 @@ $helper = $fb->getRedirectLoginHelper();
         header("Location: index.php");
         
     } catch (Facebook\Exceptions\FacebookResponseException $e) {
-        // When Graph returns an error
         echo 'Graph returned an error: ' . $e->getMessage();
     } catch (Facebook\Exceptions\FacebookSDKException $e) {
-        // When validation fails or other local issues
         echo 'Facebook SDK returned an error: ' . $e->getMessage();
     }
